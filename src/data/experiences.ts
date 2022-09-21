5973ab42c2c208ba2dda1e25cf1efec20fed82e8
Kevin Puscán Ortiz <kevinjpuscan@gmail.com>
2022-09-21 11:29:42 -0500

    update readme

--- src/data/experiences.ts
+++ src/data/experiences.ts
@@ -1,37 +1,44 @@
 export const experiences = [
   {
-    name:"Tech Lead",
-    company:"Continuum",
-    startDate:"Mar 2022",
-    endDate:"Present",
-    description:"---"
+    name: 'Tech Lead',
+    company: 'Continuum',
+    startDate: 'Mar 2022',
+    endDate: 'Present',
+    description:
+      'Lead the technical team of the relink challenge, contributing from the technical side to the growth stage of the platform.',
+    companyUrl: 'https://www.continuumhq.com',
   },
   {
-    name:"Full Stack Developer",
-    company:"Continuum",
-    startDate:"Feb 2021",
-    endDate:"Mar 2022",
-    description:"---"
+    name: 'Full Stack Developer',
+    company: 'Continuum',
+    startDate: 'Feb 2021',
+    endDate: 'Mar 2022',
+    description:
+      'Integrate the relink development team, a career guidance platform, which seeks to connect people with job opportunities aligned with their skills and interests.',
+    companyUrl: 'https://www.continuumhq.com',
   },
   {
-    name:"Remote Support Enginner",
-    company:"Hexagon Mining",
-    startDate:"Set 2020",
-    endDate:"Feb 2021",
-    description:"Provide operational support to the mining fleet management system and anti-fatigue system."
+    name: 'Remote Support Enginner',
+    company: 'Hexagon Mining',
+    startDate: 'Set 2020',
+    endDate: 'Feb 2021',
+    description: 'Provide operational support to the mining fleet management system and anti-fatigue system.',
+    companyUrl: 'https://www.hexagonmining.com',
   },
   {
-    name:"Application Analyst",
-    company:"Mine Sense For Miners",
-    startDate:"Set 2018",
-    endDate:"Set 2020",
-    description:"Provide operational support to the mining fleet management system at MinaJusta - Marcobre."
+    name: 'Application Analyst',
+    company: 'Mine Sense For Miners',
+    startDate: 'Set 2018',
+    endDate: 'Set 2020',
+    description: 'Provide operational support to the mining fleet management system at MinaJusta - Marcobre.',
+    companyUrl: 'https://www.ms4m.com',
   },
   {
-    name:"Developer",
-    company:"Mine Sense For Miners",
-    startDate:"Nov 2017",
-    endDate:"Set 2018",
-    description:"Develop systems modules in mining technology."
-  }
-]
+    name: 'Developer',
+    company: 'Mine Sense For Miners',
+    startDate: 'Nov 2017',
+    endDate: 'Set 2018',
+    description: 'Develop systems modules in mining technology.',
+    companyUrl: 'https://www.ms4m.com',
+  },
+];
